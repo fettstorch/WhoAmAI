92d8790f66b85c9333aef2ca2118f56dd1c955cc
julian <mueller@lotum.de>
2023-08-05 15:57:48 +0200

    feat: integrate vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vercel from 'vite-plugin-vercel'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vercel()],
   test: {
     includeSource: ["src/**/*.{js,ts,vue}"],
   },
