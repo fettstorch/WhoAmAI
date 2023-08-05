167507b81d2e595a5dea7c7ea6c7406da29f752e
julian <mueller@lotum.de>
2023-08-05 16:14:04 +0200

    feat(build): uninstall vite-plugin-vercel
    as it possibly ruined remote deployment :/ ?!

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vercel from 'vite-plugin-vercel'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vercel()],
+  plugins: [vue()],
   test: {
     includeSource: ["src/**/*.{js,ts,vue}"],
   },
